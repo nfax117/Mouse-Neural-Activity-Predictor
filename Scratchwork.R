d12d04aebbf10220774f5208e8137beb850d318b
nfax117 <74328491+nfax117@users.noreply.github.com>
2023-06-07 13:16:30 -0700

    Added svm, tree, and logistic predictive models

--- Scratchwork.R
+++ Scratchwork.R
@@ -1,4 +1,119 @@
 
+# Code do get brain area firing rates
+# Dataframe for the average spks per trial for each session
+
+# Function to calculate the average spks per brain area for the specific trial of a session
+firing_rate_brainarea <- function(trialNum, currSession) {
+  
+  spkTrial = currSession$spks[[trialNum]]
+  currBrainArea = currSession$brain_area
+  spkCount = apply(spkTrial, 1, sum)
+  spkAvg = tapply(spkCount, currBrainArea, mean)
+  return(spkAvg)
+  
+}
+
+
+# Function to find average spks per brain area for all trials in a session
+trial_firing_rate_brainarea <- function(sessionNum) {
+  
+  trialLen = length(session[[sessionNum]]$feedback_type)
+  brainareaLen = length(unique(session[[sessionNum]]$brain_area))
+  
+  trialBrainareaSummary = matrix(nrow = trialLen, ncol = brainareaLen + 2)
+  
+  for(q in 1:trialLen) {
+    
+    trialBrainareaSummary[q,] = c(firing_rate_brainarea(q, currSession = session[[sessionNum]]), sessionNum, q)
+    
+  }
+  
+  colnames(trialBrainareaSummary) = c(names(firing_rate_brainarea(q,currSession = session[[sessionNum]])), 'session_number', 'trial_number')
+  
+  trialBrainareaSummary <- as_tibble(trialBrainareaSummary)
+  
+  
+  # Plot the spikes per area in each session against trials
+  colorPalette = brewer.pal(n=brainareaLen, name = 'Paired')
+  
+  plot(0~1, col='white',xlim=c(0,trialLen),ylim=c(0.5,2.2), xlab="Trials",ylab="Average Spike Counts", main=paste("Spikes per Area in Session", sessionNum))
+  
+  for(i in 1:brainareaLen){
+    lines(y=trialBrainareaSummary[[i]],x=trialBrainareaSummary$trial_number,col=colorPalette[i],lty=2,lwd=1)
+    lines(smooth.spline(trialBrainareaSummary$trial_number, trialBrainareaSummary[[i]]),col=colorPalette[i],lwd=3)
+  }
+  
+  legend("topright", 
+         legend = colnames(trialBrainareaSummary)[1:brainareaLen], 
+         col = colorPalette, 
+         lty = 1, 
+         cex = 0.8
+  )
+  
+  
+  # Plot the correlation matrix for the brain areas, see if there are relations between brain areas
+  correlationMatrixPlot <- suppressWarnings(chart.Correlation(trialBrainareaSummary[, 3:brainareaLen], histogram = TRUE, pch = 7, alpha = 0.3))
+  correlationMatrixPlot
+  
+  
+  # Return the dataframe of brain areas for one session
+  return(trialBrainareaSummary)
+  
+}
+
+# Use above function to apply to all sessions, bind them together to make one dataframe of all session brainarea spk averages. 
+
+brainarea_firing_rateDF <- data.frame()
+
+for (p in 1:18) {
+  
+  brainarea_firing_rateDF = bind_rows(brainarea_firing_rateDF, trial_firing_rate_brainarea(p))
+  
+}
+
+brainarea_firing_rateDF <- relocate(brainarea_firing_rateDF, trial_number)
+brainarea_firing_rateDF <- relocate(brainarea_firing_rateDF, session_number)
+
+#head(brainarea_firing_rateDF)
+#datatable(brainarea_firing_rateDF)
+
+# In the correlation matrices:
+#The distribution of each variable is shown on the diagonal.
+#On the bottom of the diagonal : the bivariate scatter plots with a fitted line are displayed
+#On the top of the diagonal : the value of the correlation plus the significance level as stars
+#Each significance level is associated to a symbol : p-values(0, 0.001, 0.01, 0.05, 0.1, 1) <=> symbols(“***”, “**”, “*”, “.”, " “)
+
+############
+
+# Scatterplot of mice brainarea firing rate by trial per mouse
+
+tempBrainAreaPerMouse <- completeDF[completeDF$mouse_name == mouseNameVector[4], ]
+tempBrainAreaPerMouse <- tempBrainAreaPerMouse[,colSums(is.na(tempBrainAreaPerMouse))<nrow(tempBrainAreaPerMouse)]
+tempBrainAreaPerMouse <- tempBrainAreaPerMouse[, 14:length(colnames(tempBrainAreaPerMouse))]
+tempBrainAreaPerMouse[is.na(tempBrainAreaPerMouse)] <- 0
+
+ggplot(melt(tempBrainAreaPerMouse), aes(x = variable, y = value)) + geom_boxplot(outlier.color="red", outlier.shape=8, outlier.size=2, alpha = 0.2) + xlab("Brain Areas") + ylab("Firing Rate") + ggtitle(paste("Mouse", mouseNameVector[4], "Box Plots of Brain Area Firing Rates"))
+
+
+
+# Scatterplot for Mice Brain areas on feedback type per mouse
+
+for (i in 1:18) {
+  
+  tempBrainAreaDF <- completeDF[completeDF$session_number == i, colSums(is.na(completeDF[completeDF$session_number == i,])) == 0]
+  tempBrainAreaDF <- melt(tempBrainAreaDF, id.vars = "feedback_type", measure.vars = colnames(tempBrainAreaDF)[14:length(colnames(tempBrainAreaDF))])
+  
+  
+  tempPlot <- ggplot(tempBrainAreaDF) + geom_boxplot(aes(x = as.factor(feedback_type), y = value, color = variable), outlier.color="red", outlier.shape=8, outlier.size=2, alpha = 0.2) + xlab("Feedback Type") + ylab("Firing Rate") + ggtitle(paste("Session", i, "Box Plot of Brain Area Firing Rate on Feedback Type")) + guides(color = guide_legend(title = "Brain Areas"))
+  
+  print(tempPlot)
+  
+}
+
+
+############
+
+
 # Perform KMeans clustering
 
 # MAY NEED TO DO THIS WITH THE COMPLETE DATAFRAME INSTEAD OF CLUSTER DATAFRAME
